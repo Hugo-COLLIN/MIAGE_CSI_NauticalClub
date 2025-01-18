7ef51f92abed2eeb2f69ef878cc51f01498ba663
Hugo COLLIN <hugo.collin1@outlook.fr>
2025-01-18 11:47:51 +0100

    feat: adding cours page

--- src/services/crud-api/config-tables.ts
+++ src/services/crud-api/config-tables.ts
@@ -6,5 +6,32 @@ export const TABLES_CONFIG: Record<string, TableConfig> = {
     idColumn: 'id_client',
     requiredFields: ['nom', 'prenom', 'email', 'telephone'],
     allowedFields: ['nom', 'prenom', 'email', 'telephone', 'piece_identite', 'id_camping']
+  },
+  cours: {
+    tableName: 'Cours',
+    idColumn: 'id_cours',
+    requiredFields: [
+      'date_cours',
+      'heure_debut',
+      'niveau',
+      'nb_inscrits',
+      'statut',
+      'capacite_max',
+      'id_saison',
+      'id_forfait',
+      'id_personnel'
+    ],
+    allowedFields: [
+      'date_cours',
+      'heure_debut',
+      'niveau',
+      'nb_inscrits',
+      'statut',
+      'capacite_max',
+      'id_saison',
+      'id_forfait',
+      'id_personnel'
+    ]
   }
+  // Add more tables here
 };
